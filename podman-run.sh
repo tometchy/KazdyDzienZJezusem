49d38f8998210040d7ed831072919360263ad401
Tometchy <tometchy@gmail.com>
2026-04-12 11:08:25 +0200

    Test

--- podman-run.sh
+++ podman-run.sh
@@ -1,6 +1,16 @@
+#!/bin/sh
+
+echo "TR"
 podman run --rm textus-redis jhn1,1 tr
+
+echo "TNP"
 podman run --rm textus-redis jhn1,1 tnp
+
+echo "UBG"
 podman run --rm textus-redis jhn1,1 ubg
+
+echo "KJV"
 podman run --rm textus-redis jhn1,1 kjv
-echo 'Now all'
+
+echo "ALL"
 podman run --rm textus-redis jhn1,1 all
