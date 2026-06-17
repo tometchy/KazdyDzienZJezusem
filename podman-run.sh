b36d9809fab0bf4aa160a76868528696d25ca5c4
Tometchy <tometchy@gmail.com>
2026-06-17 12:40:04 +0200

    Data out

--- podman-run.sh
+++ podman-run.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-# SCRIPT_DIR="/home/tom/Projects/KazdyDzienZJezusem"
-#podman run --rm \
-#  -v "$SCRIPT_DIR":/data-out \
-#  kazdy-dzien $1
-
+SCRIPT_DIR="/home/tom/Projects/KazdyDzienZJezusem"
 
 podman run --rm \
+  -v "$SCRIPT_DIR":/data-out \
   kazdy-dzien $1
+
+
+# podman run --rm \
+#  kazdy-dzien $1
