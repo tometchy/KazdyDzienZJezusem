4c9b47a8fabff29e55339f392fd6926248408ba2
Tometchy <tometchy@gmail.com>
2026-07-13 01:17:56 +0200

    Push image

--- podman-build.sh
+++ podman-build.sh
@@ -1,2 +1,5 @@
 #podman build -t kazdy-dzien . --no-cache -f KazdyDzienZJezusem/Dockerfile
-podman build -t kazdy-dzien . -f KazdyDzienZJezusem/Dockerfile
+
+IMAGE=ghcr.io/tometchy/kazdydzienzjezusem
+podman build -t kazdy-dzien . -f KazdyDzienZJezusem/Dockerfile -t "$IMAGE:latest"
+podman push --authfile ~/.config/containers/auth.json "$IMAGE:latest"
