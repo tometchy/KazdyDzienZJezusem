314268760db55441ec1d33d3b890df0edbcff275
Tometchy <tometchy@gmail.com>
2026-08-19 20:15:11 +0200

    Clean MVC

---
+++ KazdyDzienZJezusem/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace KazdyDzienZJezusem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
